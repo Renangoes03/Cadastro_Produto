d5caa46e9784ac6a0b9e954a64bb5178770fa7d1
Renangoes03~ <renangoessantos9@gmail.com>
2023-05-08 19:42:13 -0300

    Lista de Produtos Carregadas

---
+++ src/app/produto.ts
@@ -0,0 +1,12 @@
+export interface Produto {
+    ID : number,
+    Nome : string,
+    Marca : string,
+    Aro : number,
+    Modelo : string
+    Medida : string,
+    Largura : string
+    Altura : string,
+    Tamanho : string,
+     
+}
